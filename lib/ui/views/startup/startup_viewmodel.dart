aa51bbf99a7f4ce3095afd7433517474872568a1
safauludogan <suludogan95@gmail.com>
2025-03-24 09:18:20 +0300

    /#75

--- lib/ui/views/startup/startup_viewmodel.dart
+++ lib/ui/views/startup/startup_viewmodel.dart
@@ -10,7 +10,7 @@ class StartupViewModel extends BaseViewModel {
 
   // Place anything here that needs to happen before we get into the application
   Future runStartupLogic() async {
-    await Future.delayed(const Duration(milliseconds: 500));
+    await Future<void>.delayed(const Duration(milliseconds: 1500));
 
     // This is where you can make decisions on where your app should navigate when
     // you have custom startup logic
--- module/widgets/lib/src/utility/shimmer/custom_shimmer.dart
+++ module/widgets/lib/src/utility/shimmer/custom_shimmer.dart
@@ -41,28 +41,6 @@ final class CustomShimmer extends StatelessWidget {
     );
   }
 
+  /// Shimmer card
   Widget get shimmerCard => Container(color: Colors.white);
 }
-
-/* 
-class CustomShimmer extends StatelessWidget {
-  const CustomShimmer({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: 200.0,
-      height: 100.0,
-      child: Shimmer.fromColors(
-        baseColor: Colors.red,
-        highlightColor: Colors.yellow,
-        child: Text(
-          'Shimmer',
-          textAlign: TextAlign.center,
-          style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold),
-        ),
-      ),
-    );
-  }
-}
- */
